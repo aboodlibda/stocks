f47944c486f1e877b0ad392143beaeb71981694b
aboodlibda <aboodlibda@gmail.com>
2025-03-17 11:36:33 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,25 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\StockController;
+use App\Models\Company;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+
+    Route::get('/stock', [StockController::class, 'getStock']);
+    Route::get('market-stock-screen', [HomeController::class, 'marketStockScreen'])->name('market-stock-screen');
+    Route::get('/get-stock-averages', [HomeController::class, 'getStockAverages'])->name('get-stock-averages');
+    Route::get('/companies/ajax', [HomeController::class, 'ajaxIndex'])->name('companies.index.ajax');
+    Route::get('test', function () {
+        return view('cms.test');
+    });
     Route::group(['prefix' => 'cms', 'middleware' => ['auth:user']], function () {
         Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
         Route::resource('portfolios', PortfolioController::class);
+
+
     });
 
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
