e61141cd8aba803c14f0ad8261cad85ecf1f9644
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-03-23 14:42:14 +0200

    Update edit.blade.php

--- resources/views/cms/portfolio/edit.blade.php
+++ resources/views/cms/portfolio/edit.blade.php
@@ -1,5 +1,5 @@
 @extends('cms.layout.master')
-@section('title',trans('dashboard_trans.Edit Coupon'))
+@section('title',trans('dashboard_trans.edit portfolio'))
 @section('content')
     <!--begin::Page title-->
     <div class="page-title d-flex flex-column align-items-start justify-content-center flex-wrap me-lg-2 pb-10 pb-lg-0" data-kt-swapper="true" data-kt-swapper-mode="prepend" data-kt-swapper-parent="{default: '#kt_content_container', lg: '#kt_header_container'}">
@@ -12,8 +12,8 @@
                 <a href="{{ route('dashboard') }}" class="text-muted text-hover-primary">{{trans('dashboard_trans.Home')}}</a>
             </li>
             <li class="breadcrumb-item text-muted">{{trans('dashboard_trans.Dashboard')}}</li>
-            <li class="breadcrumb-item text-muted"><a class="text-muted text-hover-primary" href="{{ route('coupons.index') }}">{{trans('dashboard_trans.Coupons')}}</a></li>
-            <li class="breadcrumb-item text-dark">{{trans('dashboard_trans.Edit Coupon')}}</li>
+            <li class="breadcrumb-item text-muted"><a class="text-muted text-hover-primary" href="{{ route('portfolios.index') }}">{{trans('dashboard_trans.portfolios')}}</a></li>
+            <li class="breadcrumb-item text-dark">{{trans('dashboard_trans.edit portfolio')}}</li>
         </ul>
         <!--end::Breadcrumb-->
     </div>
@@ -23,7 +23,7 @@
         <!--begin::Container-->
         <div class="container-xxl" id="kt_content_container">
             <!--begin::Form-->
-            <form id="kt_ecommerce_edit_coupon_form" action="{{route('coupons.update',$coupon->id)}}" class="form d-flex flex-column flex-lg-row" data-kt-redirect="{{route('coupons.edit',$coupon->id)}}">
+            <form id="kt_ecommerce_edit_coupon_form" method="POST" action="{{route('portfolios.update',$portfolio->id)}}"  class="form d-flex flex-column flex-lg-row" data-kt-redirect="{{route('portfolios.edit',$portfolio->id)}}">
                 @csrf
                 @method('PUT')
                 <!--begin::Main column-->
@@ -33,12 +33,11 @@
                         <!--begin::Tab pane-->
                         <div class="tab-pane fade show active" id="kt_ecommerce_edit_coupon_general" role="tab-panel">
                             <div class="d-flex flex-column gap-7 gap-lg-10">
-                                <!--begin::General options-->
                                 <div class="card card-flush py-4">
                                     <!--begin::Card header-->
                                     <div class="card-header">
                                         <div class="card-title">
-                                            <h2>{{trans('dashboard_trans.General')}}</h2>
+                                            <h2>{{trans('dashboard_trans.Portfolio data')}}</h2>
                                         </div>
                                     </div>
                                     <!--end::Card header-->
@@ -47,142 +46,51 @@
                                         <!--begin::Input group-->
                                         <div class="mb-10 fv-row">
                                             <!--begin::Label-->
-                                            <label class="required form-label">{{trans('dashboard_trans.Coupon')}}</label>
+                                            <label class="required form-label">{{trans('dashboard_trans.portfolio')}}</label>
                                             <!--end::Label-->
                                             <!--begin::Input-->
-                                            <input type="text" name="code" class="form-control mb-2" placeholder="{{trans('dashboard_trans.Coupon Code')}}" value="{{$coupon->code}}" />
+                                            <input type="text" name="name" class="form-control rounded-4 mb-2" placeholder="{{trans('dashboard_trans.Name')}}" value="{{$portfolio->name}}" />
                                             <!--end::Input-->
                                             <!--begin::Description-->
-                                            <div class="text-muted fs-7">{{trans('dashboard_trans.A coupon code is required and recommended to be unique')}}.</div>
+                                            <div class="text-muted fs-7">{{trans('dashboard_trans.Portfolio name is recommended to be unique')}}.</div>
                                             <!--end::Description-->
-                                            <div id="code-error" class="error-message" style="color: red;"></div>
+                                            <div id="name-error" class="error-message" style="color: red;"></div>
                                         </div>
-                                        <!--end::Input group-->
-                                        <!--begin::Input group-->
-                                        @foreach(config('lang') as $key => $lang)
-                                            <div class="col-md-12 mb-5 fv-row">
-                                                <!--begin::Label-->
-                                                <label class="form-label">{{trans('dashboard_trans.Description')}} ({{$lang}})</label>
-                                                <!--end::Label-->
-                                                <!--begin::Editor-->
-                                                <div>
-                                                    <textarea name="description[{{$key}}]" class="form-control @error('description') is-invalid @enderror">{{$coupon->getTranslation('description',$key)}}</textarea>
-                                                </div>
-                                                <!--end::Editor-->
-                                                <div id="description-{{$key}}-error" class="error-message"></div>
-                                            </div>
-                                        @endforeach
-                                        <!--end::Input group-->
-                                        <!--begin::Tax-->
-                                        <div class="d-flex flex-wrap gap-5">
-                                            <!--begin::Input group-->
-                                            <div class="fv-row w-100 flex-md-root">
-                                                <!--begin::Label-->
-                                                <label class="required form-label">{{trans('dashboard_trans.Discount Type')}}</label>
-                                                <!--end::Label-->
-                                                <!--begin::Input-->
-                                                <select class="form-select mb-2" data-control="select2" name="discount_type" data-hide-search="true" data-placeholder="{{trans('dashboard_trans.Select an option')}}">
-                                                    <option></option>
-                                                    <option value="percentage" @selected($coupon->discount_type == 'percentage')>{{trans('dashboard_trans.Percentage')}}</option>
-                                                    <option value="fixed" @selected($coupon->discount_type == 'fixed')>{{trans('dashboard_trans.Fixed')}}</option>
-                                                </select>
-                                                <!--end::Input-->
-                                            </div>
-                                            <!--end::Input group-->
-                                            <!--begin::Input group-->
-                                            <div class="fv-row w-100 flex-md-root">
-                                                <!--begin::Label-->
-                                                <label class="required form-label">{{trans('dashboard_trans.Discount Value')}}</label>
-                                                <!--end::Label-->
-                                                <!--begin::Input-->
-                                                <input type="text" name="discount_value" class="form-control mb-2" placeholder="{{trans('dashboard_trans.Discount Value')}}" value="{{$coupon->discount_value}}" />
-                                                <!--end::Input-->
-                                            </div>
-                                            <!--end::Input group-->
 
-                                        </div>
-                                        <!--end:Tax-->
-                                        <!--begin::Tax-->
-                                        <div class="d-flex flex-wrap gap-5">
-                                            <!--begin::Input group-->
-                                            <div class="fv-row w-100 flex-md-root">
-                                                <!--begin::Label-->
-                                                <label class="required form-label">{{trans('dashboard_trans.Times Used')}}</label>
-                                                <!--end::Label-->
-                                                <!--begin::Input-->
-                                                <input type="number" name="times_used" class="form-control mb-2" placeholder="{{trans('dashboard_trans.Times Used')}}" value="{{$coupon->times_used}}" />
-                                                <!--end::Input-->
-                                            </div>
-                                            <!--end::Input group-->
-                                            <!--begin::Input group-->
-                                            <div class="fv-row w-100 flex-md-root">
-                                                <!--begin::Label-->
-                                                <label class="required form-label">{{trans('dashboard_trans.Max Usage')}}</label>
-                                                <!--end::Label-->
-                                                <!--begin::Input-->
-                                                <input type="number" name="max_used" placeholder="{{trans('dashboard_trans.Max Usage')}}" class="form-control mb-2" value="{{$coupon->max_used}}" />
-                                                <!--end::Input-->
-                                            </div>
-                                            <!--end::Input group-->
-                                        </div>
-                                        <!--end:Tax-->
-                                        <!--begin::Input group-->
-                                        <div class="fv-row">
+                                        <div class="mb-10 fv-row">
                                             <!--begin::Label-->
-                                            <label class="required form-label">{{trans('dashboard_trans.Start Date')}}</label>
+                                            <label class="required form-label">{{trans('dashboard_trans.Investment amount')}}</label>
                                             <!--end::Label-->
-                                            <!--begin::Editor-->
-                                            <input type="date" name="start_date" placeholder="{{trans('dashboard_trans.Start Date')}}" class="form-control mb-2" value="{{$coupon->start_date}}" />
-                                            <!--end::Editor-->
+                                            <!--begin::Input-->
+                                            <input type="text" name="investment_amount" class="form-control rounded-4 mb-2" placeholder="{{trans('dashboard_trans.Investment amount')}}" value="{{$portfolio->investment_amount}}" />
+                                            <!--end::Input-->
+                                            <div id="investment_amount-error" class="error-message" style="color: red;"></div>
                                         </div>
-                                        <!--end::Input group-->
-                                        <!--begin::Input group-->
-                                        <div class="fv-row">
+
+
+                                        <div class="mb-10 fv-row">
                                             <!--begin::Label-->
-                                            <label class="required form-label">{{trans('dashboard_trans.End Date')}}</label>
+                                            <label class="required form-label">{{trans('dashboard_trans.Status')}}</label>
                                             <!--end::Label-->
-                                            <!--begin::Editor-->
-                                            <input type="date"  name="end_date" placeholder="{{trans('dashboard_trans.End Date')}}" class="form-control mb-2" value="{{$coupon->end_date}}" />
-                                            <!--end::Editor-->
+                                            <!--begin::Input-->
+                                            <select class="form-select mb-2" name="status" data-control="select2"   data-hide-search="true" data-placeholder="{{trans('dashboard_trans.Select an option')}}" id="kt_ecommerce_edit_coupon_status_select">
+                                                <option></option>
+                                                <option value="active" @selected($portfolio->status == 'active')>{{trans('dashboard_trans.Active')}}</option>
+                                                <option value="inactive" @selected($portfolio->status == 'inactive')>{{trans('dashboard_trans.Inactive')}}</option>
+                                            </select>
+                                            <!--end::Select2-->
+                                            <!--begin::Description-->
+                                            <div class="text-muted fs-7">{{trans('dashboard_trans.Set the portfolio status')}}.</div>
+                                            <!--end::Description-->
                                         </div>
+                                        <div id="status-error" class="error-message"></div>
                                         <!--end::Input group-->
+
+
                                         <!--end::Card header-->
                                     </div>
                                     <!--end::General options-->
                                 </div>
-                                <!--begin::Status-->
-                                <div class="card card-flush py-4">
-                                    <!--begin::Card header-->
-                                    <div class="card-header">
-                                        <!--begin::Card title-->
-                                        <div class="card-title">
-                                            <h2>{{trans('dashboard_trans.Status')}}</h2>
-                                        </div>
-                                        <!--end::Card title-->
-                                        <!--begin::Card toolbar-->
-                                        <div class="card-toolbar">
-                                            <div class="rounded-circle bg-success w-15px h-15px" id="kt_ecommerce_edit_coupon_status"></div>
-                                        </div>
-                                        <!--begin::Card toolbar-->
-                                    </div>
-                                    <!--end::Card header-->
-                                    <!--begin::Card body-->
-                                    <div class="card-body pt-0">
-                                        <!--begin::Select2-->
-                                        <select class="form-select mb-2" name="status" data-control="select2"   data-hide-search="true" data-placeholder="{{trans('dashboard_trans.Select an option')}}" id="kt_ecommerce_edit_coupon_status_select">
-                                            <option></option>
-                                            <option value="active" @selected($coupon->status == 'active')>{{trans('dashboard_trans.Active')}}</option>
-                                            <option value="inactive" @selected($coupon->status == 'inactive')>{{trans('dashboard_trans.Inactive')}}</option>
-                                        </select>
-                                        <!--end::Select2-->
-                                        <!--begin::Description-->
-                                        <div class="text-muted fs-7">{{trans('dashboard_trans.Set the category status')}}.</div>
-                                        <!--end::Description-->
-                                    </div>
-                                    <div id="status-error" class="error-message"></div>
-                                    <!--end::Card body-->
-                                </div>
-                                <!--end::Status-->
                             </div>
                             <!--end::Tab pane-->
                         </div>
@@ -191,7 +99,7 @@
 
                     <div class="d-flex justify-content-end">
                         <!--begin::Button-->
-                        <a href="{{route('coupons.index')}}" id="kt_ecommerce_edit_coupon_cancel" class="btn btn-light me-5">{{trans('dashboard_trans.Cancel')}}</a>
+                        <a href="{{route('portfolios.index')}}" id="kt_ecommerce_edit_coupon_cancel" class="btn btn-light me-5">{{trans('dashboard_trans.Cancel')}}</a>
                         <!--end::Button-->
                         <!--begin::Button-->
                         <button type="submit" id="kt_ecommerce_edit_coupon_submit" class="btn btn-primary">
@@ -212,7 +120,6 @@
 @endsection
 @section('script')
     <!--begin::Custom Javascript(used for this page only)-->
-    <script src="{{asset('assets/js/custom/apps/ecommerce/catalog/edit-coupon.js')}}"></script>
-    <script src="{{asset('assets/js/custom/utilities/modals/users-search.js')}}"></script>
+    <script src="{{asset('assets/js/custom/edit_portfolio.js')}}"></script>
     <!--end::Custom Javascript-->
 @endsection
