63d6a6582d5fd8f62c8abca7f39c1c98234c7d3d
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-03-24 16:34:19 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,10 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
     Route::get('market-stock-screen', [HomeController::class, 'marketStockScreen'])->name('market-stock-screen');
     Route::get('/get-stock-averages', [HomeController::class, 'getStockAverages'])->name('get-stock-averages');
     Route::get('/companies/ajax', [HomeController::class, 'ajaxIndex'])->name('companies.index.ajax');
-    Route::get('test', function () {
-        return view('cms.test');
-    });
+    Route::get('new-market-stock-screen', function () {
+        $companies = Company::query()->orderBy('index_name')->get();
+        return view('cms.test', compact('companies'));
+    })->name('test');
     Route::group(['prefix' => 'cms', 'middleware' => ['auth:user']], function () {
         Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
         Route::resource('portfolios', PortfolioController::class);
