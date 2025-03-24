2c5d27601534432a10f5b31da94abf6b9320932c
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-03-24 16:34:16 +0200

    Update master.blade.php

--- resources/views/cms/layout/master.blade.php
+++ resources/views/cms/layout/master.blade.php
@@ -164,6 +164,24 @@
                         </div>
                         <!--end:Menu item-->
 
+                        <!--begin:Menu item-->
+                        <div  class="menu-item ">
+                            <a class="menu-link" href="{{ route('test') }}">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+											<span class="svg-icon svg-icon-5">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+													<path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+												</svg>
+											</span>
+                                            <!--end::Svg Icon-->
+										</span>
+                                <span class="menu-title">{{'تجريبي - شاشة الأسهم المالية'}}</span>
+                            </a>
+                        </div>
+                        <!--end:Menu item-->
+
                     </div>
                     <!--end::Menu-->
                 </div>
@@ -1128,9 +1146,6 @@
                         </li>
                         <li class="menu-item">
                             <a href="#" target="_blank" class="menu-link px-2">Support</a>
-                        </li>
-                        <li class="menu-item">
-                            <a href="#" target="_blank" class="menu-link px-2">Purchase</a>
                         </li>
                     </ul>
                     <!--end::Menu-->
