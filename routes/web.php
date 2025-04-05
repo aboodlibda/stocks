9370d9a4c1c1291997755d8aa947f7427e7f57b7
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-04-05 14:18:50 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PortfolioController;
 use App\Http\Controllers\StockController;
 use App\Models\Company;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
@@ -19,6 +20,27 @@ Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
         $companies = Company::query()->orderBy('index_name')->get();
         return view('cms.test', compact('companies'));
     })->name('test');
+
+    Route::get('read-companies', [StockController::class, 'read_companies'])->name('read-companies');
+    Route::get('test_api', [StockController::class,'getApi'])->name('get-api');
+    Route::get('execute',function(){
+        test();
+//        return number_format((log(42.35/42.65))*100,2);
+//       return Company::where('company_num',3001)->first();
+    });
+
+    Route::get('new_test',function(){
+        $url = "https://finance.yahoo.com/quote/7010.SR/key-statistics?p=7010.SR";
+
+        $response = Http::withHeaders([
+            "X-RapidAPI-Host" => $this->apiHost,
+            "X-RapidAPI-Key" => $this->apiKey
+        ])->get($url);
+
+        return $response->successful() ? $response->json() : null;
+
+
+    });
     Route::group(['prefix' => 'cms', 'middleware' => ['auth:user']], function () {
         Route::get('/dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
         Route::resource('portfolios', PortfolioController::class);
