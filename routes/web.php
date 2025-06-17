53805fa813bcc29cded72ec149ded8009c3fb620
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-06-17 12:57:51 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::post('/stock-performance', [HomeController::class, 'stockPerformance'])->name('stock-performance');
+Route::get('/stock-performance', [HomeController::class, 'stockPerformance'])->name('stock-performance');
 
 Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
 
