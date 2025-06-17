074115f72fcfcfd40616a9b06ca542d56f0e23d8
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-06-17 10:49:08 +0300

    Create VerifyCsrfToken.php

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class VerifyCsrfToken
+{
+    public function handle(Request $request, Closure $next)
+    {
+        return $next($request);
+    }
+
+    protected $except = [
+        '/stock-performance',
+    ];
+}
