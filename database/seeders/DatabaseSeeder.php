efb12cf0e2ab55daa53204c60516b06ec7c26dda
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-08-18 12:41:57 +0300

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
 //        ]);
         $this->call([
 //            UserSeeder::class,
-            SectorsTableSeeder::class,
+//            SectorsTableSeeder::class,
+            NotesSeeder::class,
         ]);
     }
 }
