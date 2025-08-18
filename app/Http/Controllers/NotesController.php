79717f6d737b3bc509f88bb5f2e53ad22cf154ae
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-08-18 14:36:48 +0300

    Update NotesController.php

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -36,7 +36,19 @@ class NotesController extends Controller
         $note->update($request->validated());
 
         return redirect()->route('notes.index')
-                        ->with('success','Note updated successfully');
+            ->with('success', 'تم تحديث الملاحظات بنجاح');
     }
 
+    public function getTooltipContent(Request $request)
+    {
+        $key = $request->input('key');
+        $note = Note::first();
+
+//        dd($key);
+        if ($note && isset($note->{$key})) {
+            return response()->json($note->{$key});
+        }
+
+        return response()->json(['en' => 'Content not found.', 'ar' => 'المحتوى غير موجود.'], 404);
+    }
 }
