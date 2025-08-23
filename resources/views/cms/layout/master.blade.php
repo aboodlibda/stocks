64631cee83c0d4e9307526c831d483687c9a9f1c
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-08-23 10:52:09 +0300

    Update master.blade.php

--- resources/views/cms/layout/master.blade.php
+++ resources/views/cms/layout/master.blade.php
@@ -200,6 +200,42 @@
                         </div>
                         <!--end:Menu item-->
 
+                        <!--begin:Menu item-->
+                        <div  class="menu-item ">
+                            <a class="menu-link" href="{{ route('sector-data') }}">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+											<span class="svg-icon svg-icon-5">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+													<path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+												</svg>
+											</span>
+                                            <!--end::Svg Icon-->
+										</span>
+                                <span class="menu-title">{{'بيانات القطاعات'}}</span>
+                            </a>
+                        </div>
+                        <!--end:Menu item-->
+
+                        <!--begin:Menu item-->
+                        <div  class="menu-item ">
+                            <a class="menu-link" href="{{ route('titles.index') }}">
+										<span class="menu-icon">
+											<!--begin::Svg Icon | path: icons/duotune/arrows/arr001.svg-->
+											<span class="svg-icon svg-icon-5">
+												<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+													<path d="M14.4 11H3C2.4 11 2 11.4 2 12C2 12.6 2.4 13 3 13H14.4V11Z" fill="black" />
+													<path opacity="0.3" d="M14.4 20V4L21.7 11.3C22.1 11.7 22.1 12.3 21.7 12.7L14.4 20Z" fill="black" />
+												</svg>
+											</span>
+                                            <!--end::Svg Icon-->
+										</span>
+                                <span class="menu-title">{{'العناوين'}}</span>
+                            </a>
+                        </div>
+                        <!--end:Menu item-->
+
                     </div>
                     <!--end::Menu-->
                 </div>
@@ -306,9 +342,17 @@
                                     @foreach(LaravelLocalization::getSupportedLocales()  as $localeCode => $properties)
                                     <div class="menu-item px-3">
                                         <a href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}" hreflang="{{ $localeCode }}" class="menu-link d-flex px-5">
-												<span class="symbol symbol-20px me-4">
+
+                                            @if($localeCode == 'en')
+                                                <span class="symbol symbol-20px me-4">
+													<img class="rounded-1" src="{{asset('assets/media/flags/united-states.svg')}}" alt="" />
+                                                </span>{{ $properties['native'] }}
+                                            @else
+                                                <span class="symbol symbol-20px me-4">
 													<img class="rounded-1" src="{{asset('assets/media/flags/saudi-arabia.svg')}}" alt="" />
-												 </span>{{ $properties['native'] }}</a>
+                                                </span>{{ $properties['native'] }}
+                                            @endif
+                                            </a>
                                     </div>
                                     @endforeach
                                     <!--end::Menu item-->
