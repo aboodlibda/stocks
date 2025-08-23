ae9b92ccddc87b2c3f0f0bd2ebe08821bf5db00b
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-08-23 16:29:51 +0300

    Create ProcessSectorSeederJob.php

---
+++ app/Jobs/ProcessSectorSeederJob.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Sector;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Artisan;
+
+class ProcessSectorSeederJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Sector::truncate();
+        Artisan::call('db:seed', [
+            '--class' => 'SectorsTableSeeder',
+            '--force' => true
+        ]);
+    }
+}
