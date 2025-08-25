1c4f6992c1f69f997242ca0f15508eec3b143aad
ABDULLHA YOUSEF <aboodlibda@gmail.com>
2025-08-25 12:58:10 +0300

    Update index.blade.php

--- resources/views/cms/sector/index.blade.php
+++ resources/views/cms/sector/index.blade.php
@@ -38,46 +38,24 @@
                     <!--begin::Card title-->
                     <div class="card-title">
                         <!--begin::Search-->
-                        {{--                        <div class="d-flex align-items-center position-relative my-1">--}}
-                        {{--                            <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">--}}
-                        {{--                                <span class="path1"></span>--}}
-                        {{--                                <span class="path2"></span>--}}
-                        {{--                            </i>--}}
-                        {{--                            <input type="text" data-kt-ecommerce-coupons-filter="search" class="form-control form-control-solid w-250px ps-12" placeholder="Search Notes" />--}}
-                        {{--                        </div>--}}
-                        <div class="alert alert-custom alert-white alert-shadow gutter-b" role="alert">
-                            <div class="alert-icon">
-		<span class="svg-icon svg-icon-primary svg-icon-xl"><!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Tools/Compass.svg-->
-    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <rect x="0" y="0" width="24" height="24"></rect>
-        <path
-            d="M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z"
-            fill="#000000" opacity="0.3"></path>
-        <path
-            d="M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z"
-            fill="#000000" fill-rule="nonzero"></path>
-    </g>
-            </svg><!--end::Svg Icon--></span>
-                            </div>
-                            <div class="alert-text">
-                                البيانات المستوردة من الملف تتضمن معلومات عن القطاعات خلال فترة زمنية تمتد إلى 8 سنوات، وتشمل عدة عناصر رئيسية
-                                هي: الأسعار، الرموز، التواريخ، والأسماء، بحيث يتم تنظيمها بشكل يسهّل تحليلها ودراسة التغيرات عبر السنوات.
-                            </div>
-                            <span class="form-text text-dark">تاريخ اَخر تحديث لبيانات القطاعات  {{$lastSectorDate->diffForHumans()}} | {{$lastSectorDate->format('Y-m-d')}}</span>
 
-
+                        <div class="d-flex align-items-center position-relative my-1">
+                            <i class="ki-duotone ki-magnifier fs-3 position-absolute ms-4">
+                                <span class="path1"></span>
+                                <span class="path2"></span>
+                            </i>
+                            <input type="text" id="search" class="form-control form-control-solid border-dark w-250px ps-12" placeholder="بحث بإسم أو رمز القطاع">
                         </div>
-
                         <!--end::Search-->
                     </div>
 
                     <!--end::Card title-->
                     <!--begin::Card toolbar-->
-                    {{--                    <div class="card-toolbar flex-row-fluid justify-content-end gap-5">--}}
-                    {{--                        <!--begin::Add product-->--}}
-                    {{--                        <a href="" class="btn btn-primary">Create Note</a>--}}
-                    {{--                        <!--end::Add product-->--}}
-                    {{--                    </div>--}}
+                                        <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
+                                            <!--begin::Add product-->
+                                            <a href="{{ route('sectors.upload-page') }}" class="btn btn-primary">تحميل ملف القطاعات</a>
+                                            <!--end::Add product-->
+                                        </div>
                     <!--end::Card toolbar-->
 
                 </div>
@@ -86,21 +64,14 @@
 
                 <!--begin::Row-->
 
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3 class="mb-3">تحميل ملف Excel الخاص بالقطاعات</h3>
-
-                        <form action="{{ route('upload-sector-data') }}"
-                              class="dropzone"
-                              id="excel-dropzone"
-                              enctype="multipart/form-data">
-                            @csrf
-                        </form>
+                <div class="card-body pt-0">
+                    <!--begin::Table-->
+                    <div id="itemsTable">
+                        @include('cms.sector.partials.table', ['sectors' => $sectors])
                     </div>
-                    <span class="form-text text-danger">صيغة الملف المسموحة : .xlsx, .xls, .csv, .xlsm. الحجم الأقصى للملف: 10MB.</span>
-
+                    <!--end::Table-->
                 </div>
+
 
 
             </div>
@@ -124,13 +95,46 @@
             },
             init: function () {
                 this.on("success", function (file, response) {
-                    toastr.success("تم رفع الملف بنجاح ✅");
+                    toastr.success(response.message);
                 });
                 this.on("error", function (file, errorMessage) {
-                    toastr.error("فشل رفع الملف ❌");
+                    toastr.error(errorMessage.message);
                 });
             }
         };
+    </script>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- Live Search -->
+    <script>
+        $(document).ready(function(){
+
+            function fetch_data(query = '') {
+                $.ajax({
+                    url: "{{ route('sectors.search') }}",
+                    method: 'GET',
+                    data: { query: query },
+                    success: function(data) {
+                        $('#itemsTable').html(data);
+                    }
+                });
+            }
+
+            $('#search').on('keyup', function(){
+                let query = $(this).val();
+                fetch_data(query);
+            });
+
+            // handle pagination links (ajax load)
+            $(document).on('click', '.pagination a', function(e){
+                e.preventDefault();
+                let pageUrl = $(this).attr('href');
+                $.get(pageUrl, function(data) {
+                    $('#itemsTable').html(data);
+                });
+            });
+        });
     </script>
 
 @endsection
